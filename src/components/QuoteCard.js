240b0ae9945a11415b1c19c4db63132cd23dbafb
Brook Nega <brooknega21@gmail.com>
2022-10-28 12:30:33 -0400

    color change

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -3,7 +3,7 @@ import React from "react";
 function QuotesCard(props){
     const {quote, setAFavoriteQuote, checkClicked, clicked} = props
     return(
-        <div>
+        <div className="quotes">
             <h3>{quote.quote}</h3>
             <p>-{quote.author}</p>
             <button onClick={(e)=>setAFavoriteQuote(quote)}>like</button>
