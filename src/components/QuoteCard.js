b40ec9369445e87a8ce008318ea5b5b65d7fc979
Brook Nega <brooknega21@gmail.com>
2022-10-27 20:44:09 -0400

    json update

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -5,8 +5,8 @@ function QuotesCard(props){
     return(
         <div>
             <h3>{quote.quote}</h3>
-            <p>{quote.author}</p>
-            <button onClick={(e)=>setAFavoriteQuote(quote)}>sakdln</button>
+            <p>-{quote.author}</p>
+            <button onClick={(e)=>setAFavoriteQuote(quote)}>like</button>
         </div>
     )
 }
